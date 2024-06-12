fcb2ca6f108c08fed11d535c07a7693165b7716e
dereck1988@gmail.com <dereckdemesa@Derecks-MacBook-Pro.local>
2024-06-12 14:39:24 -0700

    feat: config/passport-config.js, controllers/auth.js

--- config/passport-config.js
+++ config/passport-config.js
@@ -1,6 +1,6 @@
 const passport = require('passport');
 const LocalStrategy = require('passport-local').Strategy;
-const user = require('./models/User')
+const user = require('../models')
 
 passport.use(new LocalStrategy(
     { usernameField: 'email' },
