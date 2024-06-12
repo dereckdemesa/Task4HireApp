911a42e48af552a0ea38fbb78c18e95ed493b366
dereck1988@gmail.com <dereckdemesa@Derecks-MacBook-Pro.local>
2024-06-12 15:07:37 -0700

    feat: controllers/auth.js, config/passport-config.js

--- config/passport-config.js
+++ config/passport-config.js
@@ -1,4 +1,4 @@
-const User = require('../models/User');
+const User = require('../models');
 const passport = require('passport');
 const LocalStrategy = require('passport-local').Strategy;
 const bcrypt = require('bcrypt');
